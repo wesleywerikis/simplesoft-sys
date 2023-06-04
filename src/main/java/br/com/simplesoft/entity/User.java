6f92e0324ac09bb224939ceebeb74ea6d756e491
Wesley Werikis <wesleywerikis@outlook.com>
2023-06-04 20:46:15 -0300

    added email attribute and notes

--- src/main/java/br/com/simplesoft/entity/User.java
+++ src/main/java/br/com/simplesoft/entity/User.java
@@ -1,10 +1,33 @@
 package br.com.simplesoft.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "tbuser")
+@Entity(name = "user")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String user;
 	private String name;
 	private String password;
-	
+	private String email;
 }
