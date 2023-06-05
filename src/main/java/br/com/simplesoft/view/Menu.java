3e7bbb7b95002b8c4b6a38343af158c2b5c12f80
Wesley Werikis <wesleywerikis@outlook.com>
2023-06-04 21:36:56 -0300

    Menu screen customization

--- src/main/java/br/com/simplesoft/view/Menu.java
+++ src/main/java/br/com/simplesoft/view/Menu.java
@@ -3,10 +3,16 @@ package br.com.simplesoft.view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Menu {
 
-	private JFrame frame;
+	private JFrame frameMenu;
 
 	/**
 	 * Launch the application.
@@ -16,7 +22,7 @@ public class Menu {
 			public void run() {
 				try {
 					Menu window = new Menu();
-					window.frame.setVisible(true);
+					window.frameMenu.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +41,31 @@ public class Menu {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameMenu = new JFrame();
+		frameMenu.setTitle("· Menu");
+		frameMenu.setResizable(false);
+		frameMenu.getContentPane().setBackground(new Color(255, 255, 255));
+		frameMenu.getContentPane().setLayout(null);
+		
+		JLabel lblMenu_Successfully = new JLabel("Logged in successfully.");
+		lblMenu_Successfully.setForeground(new Color(45, 107, 37));
+		lblMenu_Successfully.setBackground(new Color(255, 255, 255));
+		lblMenu_Successfully.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblMenu_Successfully.setBounds(96, 63, 242, 48);
+		frameMenu.getContentPane().add(lblMenu_Successfully);
+		
+		JButton btnMenu_Exit = new JButton("Exit");
+		btnMenu_Exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnMenu_Exit.setFocusable(false);
+		btnMenu_Exit.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 11));
+		btnMenu_Exit.setBackground(Color.WHITE);
+		btnMenu_Exit.setBounds(155, 134, 124, 23);
+		frameMenu.getContentPane().add(btnMenu_Exit);
+		frameMenu.setBounds(100, 100, 450, 300);
+		frameMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
 }
